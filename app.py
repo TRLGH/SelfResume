10af37ff5fd9797cc5111a8ea0b9b0119d438f9f
TRLGH <tommyrlerma@gmail.com>
2023-11-16 20:02:34 +0000

    Added dynamic updates through displaying DB through web page

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template, jsonify
-from database import load_from_db
+from database import engine, load_from_db, loads_from_db
+from sqlalchemy import text
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  result_dict = load_from_db()
+  projects = load_from_db()
   return render_template('home.html',
-                         results=result_dict,
-                         company_name='Jovian')
+                         projects=projects)
 
-
+@app.route("/project/<id>")
+def show_project(id):
+  projects = loads_from_db(id)
+  if not projects:
+    return "Not Found", 404
+  return render_template('projectpage.html',
+                         projects=projects)
 
 @app.route("/api/results")
 def list_results():
--- database.py
+++ database.py
@@ -15,7 +15,17 @@ engine = create_engine(
 def load_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("SELECT * FROM projects"))
-    result_dict = []
-    # db convert to python dict 
-    result_dict = [dict(zip(result.keys(), row)) for row in result.fetchall()]
-    return result_dict
+    projects = [dict(zip(result.keys(), row)) for row in result.fetchall()]
+    return projects
+
+def loads_from_db(id):
+  with engine.connect() as conn:
+    query = text("SELECT * FROM projects WHERE id = :val")
+    result = conn.execute(query,
+    {"val":id}
+    )
+    rows = result.fetchone()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(zip(result.keys(), rows))
